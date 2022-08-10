5dccee18a0845f328a5f734367affb92d174ec81
codeyourwaytofreedom <can.kolej@gmail.com>
2022-08-10 23:33:59 +0300

    new layout with grid modal in test html to transfer the existing tables html later on
    Signed-off-by: codeyourwaytofreedom <can.kolej@gmail.com>

--- main.py
+++ main.py
@@ -84,7 +84,9 @@ def home():
         return redirect(url_for("login"))
 
 
-
+@uygulama.route('/test')
+def test():
+    return render_template("test.html")
 
 if __name__ == '__main__':
     uygulama.run(debug=True)
